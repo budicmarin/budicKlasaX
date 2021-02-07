60b0f3052fbfcb47d4ba16a15a109aaab0754a4f
budicmarin <55134480+budicmarin@users.noreply.github.com>
2021-02-07 19:59:57 +0100

    Add project files.

---
+++ KlasaX.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicKlasaX
+{
+    class KlasaX
+    {
+        public int[] arr = new int[100];
+        public int index=0;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicKlasaX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX x = new KlasaX();
+            for (int i = 0; i < 100; i++) x.arr[i] = i;
+            x.index = 50;
+            Console.WriteLine(x.arr[x.index]);
+                    
+        }
+    }
+}
